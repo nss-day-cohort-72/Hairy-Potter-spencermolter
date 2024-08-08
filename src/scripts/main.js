782fea2a5e4c704161bbc7ea843ec459927983e3
spencermolter <sacredxassassin@gmail.com>
2024-08-07 20:26:09 -0500

    Added some pottery elements and pottery function parameters

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,15 @@
 // Imports go first
-
+const { makePottery } = require('./PotteryWheel.js')
 
 
 // Make 5 pieces of pottery at the wheel
-let mug = makePottery()
+let mug = makePottery("Mug", 3, 5)
+let lamp = makePottery("Lamp", 10, 8)
+let urn = makePottery("Urn", 11, 8)
+let vase = makePottery("Vase", 12, 10)
+let plate = makePottery("Plate", 4, 4)
 
-
+console.log(mug, lamp, urn, vase, plate)
 // Fire each piece of pottery in the kiln
 
 
